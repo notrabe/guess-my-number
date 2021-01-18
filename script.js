d5d09e465a31767f80e3ad29694f3cc07c5966e6
Evan Barton <notrabe1011@gmail.com>
2021-01-17 23:02:03 -0500

    started to implement game logic

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const secretNumber = Math.trunc(Math.random() * 20 + 1)
+document.querySelector('.number').textContent = secretNumber
+
+console.log(secretNumber)
+
+document.querySelector('.check').addEventListener('click', function(){
+   const guess = Number(document.querySelector('.guess').value)
+   console.log(guess)
+
+   if(!guess){
+        document.querySelector('.message').textContent = '⛔ No number!'
+   } else if (guess === secretNumber) {
+        document.querySelector('.message').textContent = '🎉 Correct number!'
+   } else if(guess > secretNumber){
+        document.querySelector('.message').textContent = '😭 Too high!'
+   } else {
+        document.querySelector('.message').textContent = '💔 Too low!'
+   }
+})
