25a07b9c701df318b0dff8632417658ca466dbed
Evan Barton <notrabe1011@gmail.com>
2021-01-18 23:13:22 -0500

    restart button works

--- script.js
+++ script.js
@@ -9,30 +9,37 @@ document.querySelector('.score').textContent = score
 
 
 document.querySelector('.check').addEventListener('click', function(){
-   const guess = Number(document.querySelector('.guess').value)
-   console.log(guess)
+     const guess = Number(document.querySelector('.guess').value)
 
-   if(!guess){
-        document.querySelector('.message').textContent = '⛔ No number!'
-   } else if (guess === secretNumber) {
-        document.querySelector('.message').textContent = '🎉 Correct number!'
-   } else if(guess > secretNumber){
-          if(score > 1){
-               document.querySelector('.message').textContent = '😭 Too high!'
-               score--;
-               document.querySelector('.score').textContent = score
-          } else {
-               document.querySelector('.message').textContent = '😡 You lost!'
-               document.querySelector('.score').textContent = 0
+     if(!guess){
+          document.querySelector('.message').textContent = '⛔ No number!'
+     } else if (guess === secretNumber) {
+          document.querySelector('.message').textContent = '🎉 Correct number!'
+          
+          document.querySelector('body').style.backgroundColor = '#60b347'
+
+          document.querySelector('.number').style.width = '30rem'
+     } else if(guess > secretNumber){
+               if(score > 1){
+                    document.querySelector('.message').textContent = '😭 Too high!'
+                    score--;
+                    document.querySelector('.score').textContent = score
+               } else {
+                    document.querySelector('.message').textContent = '😡 You lost!'
+                    document.querySelector('.score').textContent = 0
+               }
+     } else if(guess < secretNumber) {
+               if(score > 1){
+                    document.querySelector('.message').textContent = '😢 Too low!'
+                    score--;
+                    document.querySelector('.score').textContent = score
+               } else {
+                    document.querySelector('.message').textContent = '😡 You lost!'
+                    document.querySelector('.score').textContent = 0
+               }
           }
-   } else if(guess < secretNumber) {
-          if(score > 1){
-               document.querySelector('.message').textContent = '😢 Too low!'
-               score--;
-               document.querySelector('.score').textContent = score
-          } else {
-               document.querySelector('.message').textContent = '😡 You lost!'
-               document.querySelector('.score').textContent = 0
-          }
-     }
+     })
+
+document.querySelector('.again').addEventListener('click', function (e){
+     location.reload()
 })
