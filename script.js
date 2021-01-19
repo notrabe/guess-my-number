8b1f6315234fa05a48c5765a6ed964d26302da46
Evan Barton <notrabe1011@gmail.com>
2021-01-18 23:24:49 -0500

    computer number is now hidden

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 'use strict';
 
 const secretNumber = Math.trunc(Math.random() * 20 + 1)
-document.querySelector('.number').textContent = secretNumber
-console.log(secretNumber)
 
 let score = 20
 document.querySelector('.score').textContent = score
 
 
 document.querySelector('.check').addEventListener('click', function(){
+
      const guess = Number(document.querySelector('.guess').value)
 
      if(!guess){
           document.querySelector('.message').textContent = '⛔ No number!'
      } else if (guess === secretNumber) {
           document.querySelector('.message').textContent = '🎉 Correct number!'
+
+          document.querySelector('.number').textContent = secretNumber
           
           document.querySelector('body').style.backgroundColor = '#60b347'
 
