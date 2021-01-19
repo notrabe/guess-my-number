cf42f3496c4687d80c60ecc4bd32e036009a38fe
Evan Barton <notrabe1011@gmail.com>
2021-01-18 22:30:31 -0500

    finished game logic

--- script.js
+++ script.js
@@ -2,11 +2,11 @@
 
 const secretNumber = Math.trunc(Math.random() * 20 + 1)
 document.querySelector('.number').textContent = secretNumber
+console.log(secretNumber)
 
 let score = 20
 document.querySelector('.score').textContent = score
 
-console.log(secretNumber)
 
 document.querySelector('.check').addEventListener('click', function(){
    const guess = Number(document.querySelector('.guess').value)
@@ -17,12 +17,22 @@ document.querySelector('.check').addEventListener('click', function(){
    } else if (guess === secretNumber) {
         document.querySelector('.message').textContent = '🎉 Correct number!'
    } else if(guess > secretNumber){
-        document.querySelector('.message').textContent = '😭 Too high!'
-        score--;
-        document.querySelector('.score').textContent = score
-   } else {
-        document.querySelector('.message').textContent = '💔 Too low!'
-        score--;
-        document.querySelector('.score').textContent = score
-   }
+          if(score > 1){
+               document.querySelector('.message').textContent = '😭 Too high!'
+               score--;
+               document.querySelector('.score').textContent = score
+          } else {
+               document.querySelector('.message').textContent = '😡 You lost!'
+               document.querySelector('.score').textContent = 0
+          }
+   } else if(guess < secretNumber) {
+          if(score > 1){
+               document.querySelector('.message').textContent = '😢 Too low!'
+               score--;
+               document.querySelector('.score').textContent = score
+          } else {
+               document.querySelector('.message').textContent = '😡 You lost!'
+               document.querySelector('.score').textContent = 0
+          }
+     }
 })
